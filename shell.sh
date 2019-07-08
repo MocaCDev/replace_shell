59ce03e51e10c57a11c4841029acd22a95836e51
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-07 22:09:10 -0500

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,6 +1,11 @@
-read -p "»»———————► " shell
+clear
 
-if [ $shell == 'hey' ]
+bold_green='\033[1;32m'
+
+read -p "$bold_green ——► " shell
+
+if [ $shell == 'help' ]
 then
-  echo -e "Hey"
+  echo -e "List of replace_shell commands: \nc for cd\np for pkg\na for apt\npy for python\nphp\nb for bash\ng for git\nw for wget\n
+  t-o for termux-open\ncat\np.2 for python2\ncl for clear\nr f for rm -rf File_Being_Closed\nl for ls"
 fi
