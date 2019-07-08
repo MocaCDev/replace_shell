af34d086e593d4fd60ab277935277c3de6d9e693
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-07 21:03:40 -0500

    Create file.py

---
+++ file.py
@@ -0,0 +1,9 @@
+# Shell script replacing terminal using python
+# Use python file.py when already installed with git
+# Bash file runs in the .py file with os
+# Made by ARACADERISE
+# For Termux
+
+import os
+
+os.system('sh shell.sh')
