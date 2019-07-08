78cb1bbeb20631a8fc6db3d20f1d49b51c2255a8
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-07 22:34:48 -0500

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -4,16 +4,22 @@ bold_green='\033[1;32m'
 bold_blue='\033[1;34m'
 
 echo -e "$bold_green"
-read -p "——► " shell
 
-if [ $shell == 'help' ]
-then
-  echo -e "List of replace_shell commands: \nc for cd\np for pkg\na for apt\npy for python\nphp\nb for bash\ng for git\nw for wget\n
-t-o for termux-open\ncat\np.2 for python2\ncl for clear\nr f for rm -rf File_Being_Closed\nl for ls\n\n $bold_blue Extra\n\n $bold_green
-i for import..file.py==python_code_here\n!THERE IS ONLY ONE FILE THIS FILE CAN WRITE TO!"
-elif [ $shell == 'i']
-then
-  touch file.py
-  read -p "import..file.py== " write
-  echo "$write">>file.py
-fi
+ask()
+{
+  read -p "——► " shell
+  if [ $shell == 'help' ]
+  then
+    echo -e "List of replace_shell commands: \nc for cd\np for pkg\na for apt\npy for python\nphp\nb for bash\ng for git\nw for wget\n
+  t-o for termux-open\ncat\np.2 for python2\ncl for clear\nr f for rm -rf File_Being_Closed\nl for ls\n\n $bold_blue Extra\n\n $bold_green
+  i for import..file.py==python_code_here\n!THERE IS ONLY ONE FILE THIS FILE CAN WRITE TO!"
+  fi
+  echo "$bold_green"
+  read -p "——► " shell
+  if [ $shell == 'i']
+  then
+    touch file.py
+    read -p "import..file.py== " write
+    echo "$write">>file.py
+}
+ask
