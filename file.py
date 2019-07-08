f609172ac1e75a79fc620e282b829fe7a7098250
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-07 21:37:22 -0500

    Update file.py

--- file.py
+++ file.py
@@ -6,16 +6,22 @@
 
 import os
 
+
+# Defining the main file(.py)
+file_ = open('file.py','r')
+
 class o_s:
-	def __init__(self, host, port, con):
+	def __init__(self, host, port, con, fi):
 		self.host = host
 		self.port = port
 		self.con = con
+		self.file = fi
 	def _set_(self):
 		def_port = 18080
 		self.port = def_port
 		def_host = '0.0.0.0'
 		self.host = def_host
+		self.file = file_
 		self.con = Fale
 	def run(self):
 		with p,h as self.host, self.port:
@@ -30,10 +36,8 @@ class o_s:
 # this is where shell.sh writes into the file
 # write = os.system('sh shell.sh')
 
-# Defining the main file(.py)
-file_ = open('file.py','r')
-
 with f as file_:
 	o = o_s()
 	o._get_()
 	o.run()
+	return f
