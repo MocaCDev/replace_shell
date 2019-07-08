6f64b4f90b4bc0e3ee8c18d203c955acea0393e7
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-07 21:58:48 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,3 @@
 echo "Setup.sh booted"
 apt update && apt upgrade
 pkg install python file.py
-git clone https://github.com/ARACADERISE/_Tool-Console_.git
-echo "\n==>Setting Up What Replace_Shell Requires(as all my projects do)<==\n"
-cd _Tool-Console_
-bash install.sh
-cd
-cd repace_shell
-bash shell.sh
