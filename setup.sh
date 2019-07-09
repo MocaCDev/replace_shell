5e9ac783a0069ee4cf64836fbee0b97eb8a6089f
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-08 21:18:42 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,4 +11,15 @@ if ! command -v python > /dev/null 2>&1; then
   else
     echo -e "Please download python to use the project"
   fi
+else
+  echo -e "Looks like you got everything we need, just type C to continue"
+  read -p ">> " c
+  if [ $c == 'c' ]
+  then
+    echo -e "Booting..."
+  else
+    echo -e "Error"
+    echo -e "\nLooks like you got everything we need, just type C to continue"
+    read -p ">> " c
+  fi
 fi
