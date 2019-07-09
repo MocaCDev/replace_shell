38be47b097387907f1486f644aa90ae4be69978c
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-09 18:03:30 -0500

    Update file.py

--- file.py
+++ file.py
@@ -12,58 +12,60 @@ from colorama import Fore, Style
 
 # Defining the main file(.py)
 file_ = open('file.py','r')
-
-class o_s:
-	def __init__(self, host, port,fi):
-		self.host=host
-		self.port=port
-		self.con=False
-		self.DATA = {}
-		self.file=fi
-	def _check_(self):
-		check_status=0
-		if not self.con == True:
+if 'linux' or 'posix' or 'ubuntu' or 'debian' in os.name:
+	class o_s:
+		def __init__(self, host, port,fi):
+			self.host=host
+			self.port=port
 			self.con=False
-			self.status=check_status
-		return self.status
-	def run(self):
-		_check_ = check(self.host, self.port)
-		_check_.check()
-		print('Running with host:', self.host,'\nAnd port:',self.port)
-		os.system('sh setup.sh')
-		self.DATA.update({'Host_Connection':self.host,'Port_Connection':self.port})
-		with open('host_port_data.json', 'w') as h_p_d:
-			json.dump(self.DATA,h_p_d,indent=2,sort_keys=True)
-		return self.host, self.port
-			
-# this is where shell.sh writes into the file
-# write = os.system('sh shell.sh')
+			self.DATA = {}
+			self.file=fi
+		def _check_(self):
+			check_status=0
+			if not self.con == True:
+				self.con=False
+				self.status=check_status
+			return self.status
+		def run(self):
+			_check_ = check(self.host, self.port)
+			_check_.check()
+			print('Running with host:', self.host,'\nAnd port:',self.port)
+			os.system('sh setup.sh')
+			self.DATA.update({'Host_Connection':self.host,'Port_Connection':self.port})
+			with open('host_port_data.json', 'w') as h_p_d:
+				json.dump(self.DATA,h_p_d,indent=2,sort_keys=True)
+			return self.host, self.port
 
-host = input(Fore.GREEN+Style.BRIGHT+'Host >> ')
-if host == '':
-	err = True
-	while err:
-		print(Fore.RED+'There was an error: User did not input a host')
-		host = input(Fore.GREEN+Style.BRIGHT+'Host >> ')
-		if not host == '':
-			err=False
-			break
-		else:
-			continue
-port = input(Fore.GREEN+Style.BRIGHT+'Port >> ')
-if port == '':
-	err = True
-	while err:
-		print(Fore.RED+'There was an error: User did not input a port')
-		port = input(Fore.GREEN+Style.BRIGHT+'Port >> ')
-		if not port == '':
-			err=False
-			break
-		else:
-			continue
-while not port == '' and not host == '':
-	os.system('clear')
-	o = o_s(host,port,file_)
-	o._check_()
-	o.run()
-	break
+	# this is where shell.sh writes into the file
+	# write = os.system('sh shell.sh')
+
+	host = input(Fore.GREEN+Style.BRIGHT+'Host >> ')
+	if host == '':
+		err = True
+		while err:
+			print(Fore.RED+'There was an error: User did not input a host')
+			host = input(Fore.GREEN+Style.BRIGHT+'Host >> ')
+			if not host == '':
+				err=False
+				break
+			else:
+				continue
+	port = input(Fore.GREEN+Style.BRIGHT+'Port >> ')
+	if port == '':
+		err = True
+		while err:
+			print(Fore.RED+'There was an error: User did not input a port')
+			port = input(Fore.GREEN+Style.BRIGHT+'Port >> ')
+			if not port == '':
+				err=False
+				break
+			else:
+				continue
+	while not port == '' and not host == '':
+		os.system('clear')
+		o = o_s(host,port,file_)
+		o._check_()
+		o.run()
+		break
+else:
+	print('Not available for your system')
