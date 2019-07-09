a1fe94c07877e7572cb6566aea9378406bb130c1
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-08 21:22:06 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,23 +2,23 @@ echo "Setup.sh booted"
 apt update && apt upgrade
 pkg install python file.py
 if ! command -v python > /dev/null 2>&1; then
-  echo -e "Requires Python. \n Do you want the file to download python for you?[y/n]"
+  echo "Requires Python. \n Do you want the file to download python for you?[y/n]"
   read -p ">> " u
   if [ $u == 'y' ]
   then
     pkg install python
   else
-    echo -e "Please download python to use the project"
+    echo "Please download python to use the project"
   fi
 else
-  echo -e "Looks like you got everything we need, just type C to continue"
+  echo "Looks like you got everything we need, just type C to continue"
   read -p ">> " c
   if [ $c == 'c' ]
   then
     echo -e "Booting..."
   else
-    echo -e "Error"
-    echo -e "\nLooks like you got everything we need, just type C to continue"
+    echo "Error"
+    echo "\nLooks like you got everything we need, just type C to continue"
     read -p ">> " c
   fi
 fi
