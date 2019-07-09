2b653ce2bce0ef7bcc50c75b98487d52a68a08b1
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-08 21:30:43 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+cyan='\033[1;36m'
+red='\033[1;31m'
+
 echo "Setup.sh booted"
 apt update && apt upgrade
 pkg install python file.py
@@ -11,14 +14,13 @@ if ! command -v python > /dev/null 2>&1; then
     echo "Please download python to use the project"
   fi
 else
-  echo "Looks like you got everything we need, just type C to continue"
+  echo "$cyan Looks like you got everything the project needs. Just type 'c' to continue"
   read -p ">> " c
   if [ $c == 'c' ]
   then
     echo -e "Booting..."
   else
-    echo "Error"
-    echo "\nLooks like you got everything we need, just type C to continue"
+    echo "$red Error"
     read -p ">> " c
   fi
 fi
