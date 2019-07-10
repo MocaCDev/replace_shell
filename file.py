afef77044a9d6810d48f4c71e2c2a7b38fc787ef
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-09 21:30:33 -0500

    Update file.py

--- file.py
+++ file.py
@@ -34,10 +34,12 @@ if 'linux' or 'posix' or 'ubuntu' or 'debian' in os.name and sys.platform:
 			
 			# Importing the git command to downloads
 			import_to_downloads = 'git clone https://github.com/ARACADERISE/replace_shell'
+			
 			while not os.name == False:
 				
 				# Where the git command are imported into cd downloads
-				os.system(['cd downloads',f'{import_to_downloads}'])
+				os.system('cd downloads')
+				os.system(f'{import_to_downloads}')
 				
 				os.system('sh setup.sh')
 				self.DATA.update({'Host_Connection':self.host,'Port_Connection':self.port})
