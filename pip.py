76f1630ef80770c617786d9453ae80b0dff9f2df
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-09 22:23:37 -0500

    Update pip.py

--- pip.py
+++ pip.py
@@ -1,7 +1,17 @@
-from setuptools import setup
-
-setup(
-	name='replace_shell',
-	version='1.0.1',
-	scripts=['file']
-)
+setuptools.setup(
+     name='dokr',  
+     version='0.1',
+     scripts=['dokr'] ,
+     author="Deepak Kumar",
+     author_email="deepak.kumar.iet@gmail.com",
+     description="A Docker and AWS utility package",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/javatechy/dokr",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
