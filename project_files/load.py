502b7bcbdb239f85f69841d61281a9ef9c2e20fa
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-12 14:12:20 -0500

    Update load.py

--- project_files/load.py
+++ project_files/load.py
@@ -1,4 +1,4 @@
-import os
+import os,time
 # LOADING SCREEN
 class load_project:
   def __init__(self):
@@ -7,21 +7,30 @@ class load_project:
   def _load_(self):
     while self.loading:
       print('DONWLOADING--[......................]--')
+      time.sleep(0.2)
       os.system('clear')
       print('DONWLOADING--[#.....................]-- 1%')
+      time.sleep(0.8)
       os.system('clear')
       print('DONWLOADING--[##....................]-- 2%')
+      time.sleep(0.1)
       os.system('clear')
       print('DONWLOADING--[####..................]-- 15%')
+      time.sleep(0.4)
       os.system('clear')
       print('DONWLOADING--[#######...............]-- 30%')
+      time.sleep(0.2)
       os.system('clear')
       print('DONWLOADING--[###########...........]-- 48%')
+      time.sleep(0.6)
       os.system('clear')
       print('DONWLOADING--[#################.....]-- 68%')
+      time.sleep(0.8)
       os.system('clear')
       print('DONWLOADING--[####################..]-- 88%')
+      time.sleep(0.4)
       os.system('clear')
       print('DONWLOADING--[#####################.]-- 98%')
+      time.sleep(0.2)
       os.system('clear')
       print('DONWLOADING--[######################]-- 100%')
