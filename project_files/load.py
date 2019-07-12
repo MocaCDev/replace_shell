8b37a5945b386663c7c7634aed1149abe2eb1ca3
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-12 14:25:04 -0500

    Update load.py

--- project_files/load.py
+++ project_files/load.py
@@ -1,39 +1,43 @@
 import os,time
+from colorama import Fore, Style
 # LOADING SCREEN
 class load_project:
   def __init__(self):
     self.loading=True
   def _load_(self):
     while self.loading:
-      print('DONWLOADING--[......................]--')
+      print(Fore.GREEN+Style.BRIGHT+'\n\nSetting Up--[......................]--\n\n')
       time.sleep(0.2)
       os.system('clear')
-      print('DONWLOADING--[#.....................]-- 1%')
+      print('\n\nSetting Up--[#.....................]-- 1%\n\n')
       time.sleep(0.8)
       os.system('clear')
-      print('DONWLOADING--[##....................]-- 2%')
+      print('\n\nSetting Up--[##....................]-- 2%\n\n')
       time.sleep(0.1)
       os.system('clear')
-      print('DONWLOADING--[####..................]-- 15%')
+      print('\n\nSetting Up--[####..................]-- 15%\n\n')
       time.sleep(0.4)
       os.system('clear')
-      print('DONWLOADING--[#######...............]-- 30%')
+      print('\n\nSetting Up--[#######...............]-- 30%\n\n')
       time.sleep(0.2)
       os.system('clear')
-      print('DONWLOADING--[###########...........]-- 48%')
+      print('\n\nSetting Up--[###########...........]-- 48%\n\n')
       time.sleep(0.6)
       os.system('clear')
-      print('DONWLOADING--[#################.....]-- 68%')
+      print('\n\nSetting Up--[#################.....]-- 68%\n\n')
       time.sleep(0.8)
       os.system('clear')
-      print('DONWLOADING--[####################..]-- 88%')
+      print('\n\nSetting Up--[####################..]-- 88%\n\n')
       time.sleep(0.4)
       os.system('clear')
-      print('DONWLOADING--[#####################.]-- 98%')
+      print('\n\nSetting Up--[#####################.]-- 98%\n\n')
       time.sleep(0.2)
       os.system('clear')
-      print('DONWLOADING--[######################]-- 100%')
+      print('\n\nSetting Up--[######################]-- 100%\n\n')
       self.loading=False
+      if self.loading == False:
+        os.system('echo -e "Done!"')
+        return "Done"
       
 def load():
   l_p = load_project()
