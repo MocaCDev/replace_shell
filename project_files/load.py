3d40b2ce8fbaf1a7518c31f3bcf7a2d7e9839656
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-12 13:20:48 -0500

    Update load.py

--- project_files/load.py
+++ project_files/load.py
@@ -1,6 +1,27 @@
+import os
 # LOADING SCREEN
 class load_project:
   def __init__(self):
     self.loading=True
     return self.loading
-    
+  def _load_(self):
+    while self.loading:
+      print('DONWLOADING--[......................]--')
+      os.system('clear')
+      print('DONWLOADING--[#.....................]-- 1%')
+      os.system('clear')
+      print('DONWLOADING--[##....................]-- 2%')
+      os.system('clear')
+      print('DONWLOADING--[####..................]-- 15%')
+      os.system('clear')
+      print('DONWLOADING--[#######...............]-- 30%')
+      os.system('clear')
+      print('DONWLOADING--[###########...........]-- 48%')
+      os.system('clear')
+      print('DONWLOADING--[#################.....]-- 68%')
+      os.system('clear')
+      print('DONWLOADING--[####################..]-- 88%')
+      os.system('clear')
+      print('DONWLOADING--[#####################.]-- 98%')
+      os.system('clear')
+      print('DONWLOADING--[######################]-- 100%')
