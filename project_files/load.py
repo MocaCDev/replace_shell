0bee261989a1b128a921c1a58072ec18dd3727a5
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-12 16:03:31 -0500

    Update load.py

--- project_files/load.py
+++ project_files/load.py
@@ -36,5 +36,4 @@ class load_project:
       time.sleep(0.2)
       os.system('clear')
       print('\n\nSetting Up--[######################]-- 100%\n\n')
-      self.setup_done=True
 			self.loading=False
