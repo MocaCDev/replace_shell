9e9035cb876edb04bc39464771a9131b1717f448
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-12 17:37:29 -0500

    Update file.py

--- project_files/file.py
+++ project_files/file.py
@@ -13,113 +13,115 @@ from c import check
 from colorama import Fore, Style
 import ipaddress
 from myErrors import _err_
+try:
+	def _use_mode(syst,sys):
 
-def _use_mode(syst,sys):
-	
-	def _write_(t_1,t_2):
-		write_to_file={'Device_Name':[t_1,t_2]}
-		with open('device_data.json','w') as d_d:
-			json.dump(write_to_file,d_d,indent=2,sort_keys=True)
-		
-		# .txt
-		write_in_txt = open('device.txt','w')
-		wr=f'DEVICE:{t_1},{t_2}'
-		write_in_txt.write(wr)
-		write_in_txt.close()
-		return "Done"
-	
-	if syst and sys == 'linux':
-		print('System boot in:',sys)
-		print('OS boot in:',syst)
-		def linux():
-			return _write_(syst, sys)
-		linux()
-	if syst and sys == 'posix':
-		print('System boot in:',sys)
-		print('OS boot in:',syst)
-		def posix():
-			return _write_(syst, sys)
-		posix()
-	if syst and sys == 'ubuntu':
-		print('System boot in:',sys)
-		print('OS boot in:',syst)
-		def ubuntu():
-			return _write_(syst, sys)
-		ubuntu()
-	if syst and sys == 'debian':
-		print('System boot in:',sys)
-		print('OS boot in:',syst)
-		def debian():
-			return _write_(syst, sys)
-		debian()
+		def _write_(t_1,t_2):
+			write_to_file={'Device_Name':[t_1,t_2]}
+			with open('device_data.json','w') as d_d:
+				json.dump(write_to_file,d_d,indent=2,sort_keys=True)
 
-# Defining the main file(.py)
-file_ = open('file.py','r')
+			# .txt
+			write_in_txt = open('device.txt','w')
+			wr=f'DEVICE:{t_1},{t_2}'
+			write_in_txt.write(wr)
+			write_in_txt.close()
+			return "Done"
 
-if 'linux' or 'posix' or 'ubuntu' or 'debian' in os.name and sys.platform:
-	
-	os.system('clear')
-	
-	_use_mode(os.name,sys.platform)
-	
-	class o_s:
-		def __init__(self, host, port,fi):
-			self.host=host
-			self.port=port
-			self.con=False
-			self.DATA = {}
-			self.file=fi
-		def _check_(self):
-			check_status=0
-			if not self.con == True:
-				ip()
-				self.status=check_status
-			return self.status
-		def run(self):
-			_check_ = check(self.host, self.port)
-			_check_.check()
-			print('Running with host:', self.host,'\nAnd port:',self.port)
-			
-			while not os.name == False:
-				os.system('sh setup.sh')
-				self.DATA.update({'Host_Connection':self.host,'Port_Connection':self.port})
-				with open('host_port_data.json', 'w') as h_p_d:
-					json.dump(self.DATA,h_p_d,indent=2,sort_keys=True)
-				break
-			return self.host, self.port
+		if syst and sys == 'linux':
+			print('System boot in:',sys)
+			print('OS boot in:',syst)
+			def linux():
+				return _write_(syst, sys)
+			linux()
+		if syst and sys == 'posix':
+			print('System boot in:',sys)
+			print('OS boot in:',syst)
+			def posix():
+				return _write_(syst, sys)
+			posix()
+		if syst and sys == 'ubuntu':
+			print('System boot in:',sys)
+			print('OS boot in:',syst)
+			def ubuntu():
+				return _write_(syst, sys)
+			ubuntu()
+		if syst and sys == 'debian':
+			print('System boot in:',sys)
+			print('OS boot in:',syst)
+			def debian():
+				return _write_(syst, sys)
+			debian()
 
-	# this is where shell.sh writes into the file
-	# write = os.system('sh shell.sh')
+	# Defining the main file(.py)
+	file_ = open('file.py','r')
 
-	host = input(Fore.GREEN+Style.BRIGHT+'Host >> ')
-	if host == '':
-		err = True
-		while err:
-			print(Fore.RED+'There was an error: User did not input a host')
-			host = input(Fore.GREEN+Style.BRIGHT+'Host >> ')
-			if not host == '':
-				err=False
-				break
-			else:
-				continue
-	port = input(Fore.GREEN+Style.BRIGHT+'Port >> ')
-	if port == '':
-		err = True
-		while err:
-			print(Fore.RED+'There was an error: User did not input a port')
-			port = input(Fore.GREEN+Style.BRIGHT+'Port >> ')
-			if not port == '':
-				err=False
-				break
-			else:
-				continue
-	while not port == '' and not host == '':
-		replace()
-		load()
+	if 'linux' or 'posix' or 'ubuntu' or 'debian' in os.name and sys.platform:
+
 		os.system('clear')
-		o = o_s(host,port,file_)
-		o._check_()
-		o.run()
-		break
-else:
-	raise _err_(1,'Not available for your system')
+
+		_use_mode(os.name,sys.platform)
+
+		class o_s:
+			def __init__(self, host, port,fi):
+				self.host=host
+				self.port=port
+				self.con=False
+				self.DATA = {}
+				self.file=fi
+			def _check_(self):
+				check_status=0
+				if not self.con == True:
+					ip()
+					self.status=check_status
+				return self.status
+			def run(self):
+				_check_ = check(self.host, self.port)
+				_check_.check()
+				print('Running with host:', self.host,'\nAnd port:',self.port)
+
+				while not os.name == False:
+					os.system('sh setup.sh')
+					self.DATA.update({'Host_Connection':self.host,'Port_Connection':self.port})
+					with open('host_port_data.json', 'w') as h_p_d:
+						json.dump(self.DATA,h_p_d,indent=2,sort_keys=True)
+					break
+				return self.host, self.port
+
+		# this is where shell.sh writes into the file
+		# write = os.system('sh shell.sh')
+
+		host = input(Fore.GREEN+Style.BRIGHT+'Host >> ')
+		if host == '':
+			err = True
+			while err:
+				print(Fore.RED+'There was an error: User did not input a host')
+				host = input(Fore.GREEN+Style.BRIGHT+'Host >> ')
+				if not host == '':
+					err=False
+					break
+				else:
+					continue
+		port = input(Fore.GREEN+Style.BRIGHT+'Port >> ')
+		if port == '':
+			err = True
+			while err:
+				print(Fore.RED+'There was an error: User did not input a port')
+				port = input(Fore.GREEN+Style.BRIGHT+'Port >> ')
+				if not port == '':
+					err=False
+					break
+				else:
+					continue
+		while not port == '' and not host == '':
+			replace()
+			load()
+			os.system('clear')
+			o = o_s(host,port,file_)
+			o._check_()
+			o.run()
+			break
+	else:
+		raise _err_(1,'Not available for your system')
+except KeyboardInterrupt or KeyError:
+	raise _err_(8,'Key error')
