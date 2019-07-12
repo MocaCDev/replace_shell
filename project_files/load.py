83b12aa48af694fa11fbb588438d7db89680aa8c
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-12 15:40:58 -0500

    Update load.py

--- project_files/load.py
+++ project_files/load.py
@@ -38,50 +38,50 @@ class load_project:
       print('\n\nSetting Up--[######################]-- 100%\n\n')
       self.setup_done=True
   def _get_ip(self):
-		while self.loading and not self.has_ip:
-	    print(Fore.GREEN+Style.BRIGHT+'Getting IP--[..........]--')
-	    time.sleep(1)
-	    os.system('clear')
-	    print('Getting IP--[#.........]--10%')
-	    time.sleep(0.4)
-	    os.system('clear')
-	    print('Getting IP--[##........]--20%')
-	    time.sleep(0.6)
-	    os.system('clear')
-	    print('Getting IP--[###.......]--30%')
-	    time.sleep(0.2)
-	    os.system('clear')
-	    print('Getting IP--[####......]--40%')
-	    time.sleep(0.8)
-	    os.system('clear')
-	    print('Getting IP--[#####.....]--50%')
-	    time.sleep(0.7)
-	    os.system('clear')
-	    print('Getting IP--[######....]--60%')
-	    time.sleep(1)
-	    os.system('clear')
-	    print('Getting IP--[#######...]--70%')
-	    time.sleep(0.1)
-	    os.system('clear')
-	    print('Getting IP--[########..]--80%')
-	    time.sleep(0.9)
-	    os.system('clear')
-	    print('Getting IP--[#########.]--90%')
-	    time.sleep(0.7)
-	    os.system('clear')
-	    print('Getting IP--[##########]--100%')
-	    self.set_ip = (
-		    # 107.47.80.10 ip address booted when running terminal, does not replace
-		    # real ip address. "Cover Address"
-		    ipaddress.IPv4Address('107.46.80.10'), 
-		    ipaddress.ip_network('107.46.80.10/24',strict=False),
-		    ipaddress.ip_interface('107.46.80.10/24')
-	    )
-	    self.loading=False
-	    self.has_ip=True
+    while self.loading and not self.has_ip:
+      print(Fore.GREEN+Style.BRIGHT+'Getting IP--[..........]--')
+      time.sleep(1)
+      os.system('clear')
+      print('Getting IP--[#.........]--10%')
+      time.sleep(0.4)
+      os.system('clear')
+      print('Getting IP--[##........]--20%')
+      time.sleep(0.6)
+      os.system('clear')
+      print('Getting IP--[###.......]--30%')
+      time.sleep(0.2)
+      os.system('clear')
+      print('Getting IP--[####......]--40%')
+      time.sleep(0.8)
+      os.system('clear')
+      print('Getting IP--[#####.....]--50%')
+      time.sleep(0.7)
+      os.system('clear')
+      print('Getting IP--[######....]--60%')
+      time.sleep(1)
+      os.system('clear')
+      print('Getting IP--[#######...]--70%')
+      time.sleep(0.1)
+      os.system('clear')
+      print('Getting IP--[########..]--80%')
+      time.sleep(0.9)
+      os.system('clear')
+      print('Getting IP--[#########.]--90%')
+      time.sleep(0.7)
+      os.system('clear')
+      print('Getting IP--[##########]--100%')
+      self.set_ip = (
+	      # 107.47.80.10 ip address booted when running terminal, does not replace
+        # real ip address. "Cover Address"
+	      ipaddress.IPv4Address('107.46.80.10'), 
+	      ipaddress.ip_network('107.46.80.10/24',strict=False),
+	      ipaddress.ip_interface('107.46.80.10/24')
+      )
+      self.loading=False
+      self.has_ip=True
     if self.loading == False and self.has_ip == True:
 			os.system('echo -e "Done"')
-		  return "Done"
+			return "Done"
   def show_ip(self):
     return self.set_ip
       
