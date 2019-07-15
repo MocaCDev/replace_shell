3af9465f037c4590c24f60a825833ee06a19245c
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-15 18:48:49 -0500

    Update __back__.py

--- project_files/__back__.py
+++ project_files/__back__.py
@@ -2,15 +2,16 @@ import os,sys
 # "BACKEND" SECTION
 
 CLIENT_TYPE=''
+CLIENTIN = [object]
 CLIENT=''
 
 # PLAT NAME STARTUPS
-_start_client_posix = (CLIENT == 'startup_posix')
-_start_client_KitKat = (CLIENT == 'startup_KitKat')
-_start_client_Cupcake = (CLIENT == 'startup_Cupcake')
+_start_client_posix = (CLIENT == 'startup_posix', CLIENT_TYPE == 'posix')
+_start_client_KitKat = (CLIENT == 'startup_KitKat', CLIENT_TYPE == 'KitKat')
+_start_client_Cupcake = (CLIENT == 'startup_Cupcake', CLIENT_TYPE == 'Cupcake')
 
 # LINUX STARTUP(default)
-_start_client_in_linux = (CLIENT = 'startup_linux')
+_start_client_in_linux = (CLIENT = 'startup_linux', CLIENTIN = 'linux')
 
 def __MAIN__():
   # This will steer clear of any possible(80%) issues within the future of replace_shell
@@ -38,9 +39,11 @@ def __MAIN__():
 class CREATE_CLIENT:
   def __init__(self,__type__,__mode__,__client_id__,__port___,__key__):
     self.__type__=__type__
-    self.__running__=[]
+    self.__running__= True
+    self.__ran_with__ = object
     self.__mode__=__mode__
     self.__client_id__=__client_id__
+    self.__set_client__ = [object]
     self.__port__=__port__
     self.__key__=__key__
     # MAIN PATH
@@ -59,9 +62,12 @@ class CREATE_CLIENT:
             op.write('__mode__ '+str(self.__mode__) + '\n' + '__client_id__ ' + str(self.__client_id__))
             op.close()
             return self.__mode__,self.__client_id__
-          if os.path.exists(f'{self.__usr_path__}':
-            if os.path.exists(f'{self.__usr_path__}/ssh'):
-              return self.__running__.append('ssh')         
+          # The client will then use ssh
+          if os.path.exists(f'{self.__usr_etc_path__}':
+            if os.path.exists(f'{self.__usr_etc_path__}/ssh'):
+              self.__ran_with__ = 'ssh'
+              self.__set_client__.append(self.__ran_with__)
+              return self.__running__       
  def __sort__(m,i,p):
   CREATE_CLIENT('project_client',m,i,p,'a01b26')
   return "Client Value Started With Status", 1078
