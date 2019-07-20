2a49b95464fd9eb98ecea22a2989092ecbea11fa
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-20 16:36:00 -0500

    Update setup_run.sh

--- project_files/setup_run.sh
+++ project_files/setup_run.sh
@@ -1,10 +1,12 @@
+# THIS WILL ROOT "run" TO THE /bin DIRECTORY
 if [ ! -d /data/data/com.termux/files/usr/bin/run ]; then
   mv -v run /data/data/com.termux/files/usr/bin
   chmod +x /data/data/com.termux/files/usr/bin/run
   cd
-  ls
   echo "==> Type 'run' to run replace_shell!"
 fi
+
+# IF "run" IS ALREADY LOCATED IN THE /bin DIRECTORY IT WILL PRINT YOU THIS WARNING/ALERT MESSAGE
 if [ -d /data/data/com.termux/files/usr/bin/run ]; then
   echo "==> Looks like 'run' is already rooted to your bin directory. \nType 'run' to run replace_shell!"
 fi
