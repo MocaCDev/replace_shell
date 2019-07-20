ccc2263a89828a24cc6ea925a8214031a2fcccfb
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-20 16:33:21 -0500

    Update setup_run.sh

--- project_files/setup_run.sh
+++ project_files/setup_run.sh
@@ -1,7 +1,8 @@
 if [ ! -d /data/data/com.termux/files/usr/bin/run ]; then
   mv -v run /data/data/com.termux/files/usr/bin
   chmod +x /data/data/com.termux/files/usr/bin/run
-  cd
+  cd /
+  ls
   echo "==> Type 'run' to run replace_shell'!"
 fi
 if [ -d /data/data/com.termux/files/usr/bin/run ]; then
