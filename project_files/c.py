864d475078cbae38d7b3b0d093a3e98cfedf34d4
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-22 17:57:33 -0500

    Update c.py

--- project_files/c.py
+++ project_files/c.py
@@ -8,15 +8,39 @@ class check:
     self.port=port
    
   def check(self):
-    if len(self.host) < 7:
-      err=True
+    if not self.host[1] == '.' and not self.host[3] == '.' and not self.host[5] == '.':
+      err = True
       while err:
-        print(Fore.RED+'Error: Host too short. at least 3 dots and 4 numbers')
-        host = input(Fore.GREEN+Style.BRIGHT+'Host >> ')
-        if not len(host) < 7:
+        print(Fore.RED+'Error: Script did not detect 3 dots in the host')
+        host = input(Fore.GREEN+Style.BRIGHT+'Host(3 dots, 4 numbers) >> ')
+        if host[1] == '.' and host[3] == '.' and host[5] == '.' and not len(host) > 7 and not len(host) < 7:
           self.host=host
           err=False
           break
+        else:
+          continue
+    if len(self.host) < 7:
+      err=True
+      while err:
+        print(Fore.RED+'Error: Host too short. 3 dots and 4 numbers')
+        host = input(Fore.GREEN+Style.BRIGHT+'Host(3 dots, 4 numbers) >> ')
+        if host[1] == '.' and host[3] == '.' and host[5] == '.' and not len(host) > 7 and not len(host) < 7:
+          self.host=host
+          err=False
+          break
+        else:
+          continue
+    if len(self.host) > 7:
+      err = True
+      while err:
+        print(Fore.RED+'Error: Host too long. 3 dots, 4 numbers')
+        host = input(Fore.GREEN+Style.BRIGHT+'Host(3 dots, 4 numbers) >> ')
+        if host[1] == '.' and host[3] == '.' and host[5] == '.' and not len(host) > 7 and not len(host) < 7:
+          self.host=host
+          err=False
+          break
+        else:
+          continue
     if len(self.port) < 4:
       err=True
       while err:
@@ -26,3 +50,5 @@ class check:
           self.port=port
           err=False
           break
+        else:
+          continue
