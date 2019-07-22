c8706a1b91f66ebccb74fe5fdf2cdce883eaa461
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-07-21 22:56:01 -0500

    Update set.sh

--- project_files/set.sh
+++ project_files/set.sh
@@ -1,13 +1,13 @@
 # THIS WILL ROOT "run" TO THE /bin DIRECTORY
-if [ ! -d /data/data/com.termux/files/usr/bin/replace_shell ]; then
-  mv -v replace_shell /data/data/com.termux/files/usr/bin
-  chmod +x /data/data/com.termux/files/usr/bin/replace_shell
-  echo "==> Type 'replace_shell' to run replace_shell!"
+if [ ! -d /data/data/com.termux/files/usr/bin/rep ]; then
+  mv -v rep /data/data/com.termux/files/usr/bin
+  chmod +x /data/data/com.termux/files/usr/bin/rep
+  echo "==> Type 'rep' to run replace_shell!"
   cd
   exit
 fi
 
 # IF "run" IS ALREADY LOCATED IN THE /bin DIRECTORY IT WILL PRINT YOU THIS WARNING/ALERT MESSAGE
-if [ -d /data/data/com.termux/files/usr/bin/replace_shell ]; then
-  echo "==> Looks like 'run' is already rooted to your bin directory. \nType 'run' to run replace_shell!"
+if [ -d /data/data/com.termux/files/usr/bin/rep ]; then
+  echo "==> Looks like 'run' is already rooted to your bin directory. \nType 'rep' to run replace_shell!"
 fi
