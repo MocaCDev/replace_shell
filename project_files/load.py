40ffd066b8f69379543a1bacdafc6e2d45730177
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-22 21:58:39 -0500

    Update load.py

--- project_files/load.py
+++ project_files/load.py
@@ -1,5 +1,8 @@
+# This will give buffer time to the projects files
+
 import os,time
 from colorama import Fore, Style
+from __LOGO__ import LOGO
 # LOADING SCREEN
 class load_project:
   def __init__(self):
@@ -11,30 +14,39 @@ class load_project:
       print(Fore.GREEN+Style.BRIGHT+'\n\nSetting Up--[......................]--\n\n')
       time.sleep(0.2)
       os.system('clear')
+      LOGO()
       print('\n\nSetting Up--[#.....................]-- 1%\n\n')
       time.sleep(0.8)
       os.system('clear')
+      LOGO()
       print('\n\nSetting Up--[##....................]-- 2%\n\n')
       time.sleep(0.1)
       os.system('clear')
+      LOGO()
       print('\n\nSetting Up--[####..................]-- 15%\n\n')
       time.sleep(0.4)
       os.system('clear')
+      LOGO()
       print('\n\nSetting Up--[#######...............]-- 30%\n\n')
       time.sleep(0.2)
       os.system('clear')
+      LOGO()
       print('\n\nSetting Up--[###########...........]-- 48%\n\n')
       time.sleep(0.6)
       os.system('clear')
+      LOGO()
       print('\n\nSetting Up--[#################.....]-- 68%\n\n')
       time.sleep(0.8)
       os.system('clear')
+      LOGO()
       print('\n\nSetting Up--[####################..]-- 88%\n\n')
       time.sleep(0.4)
       os.system('clear')
+      LOGO()
       print('\n\nSetting Up--[#####################.]-- 98%\n\n')
       time.sleep(0.2)
       os.system('clear')
+      LOGO()
       print('\n\nSetting Up--[######################]-- 100%\n\n')
       #os.system('pip install replace_shell')
       loaded = open('file_loaded','w')
