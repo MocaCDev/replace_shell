6539c8e9e05bd071ffad3a3ec6189df599903360
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-22 21:46:42 -0500

    Create __LOGO__.py

---
+++ project_files/__LOGO__.py
@@ -0,0 +1,15 @@
+from colorama import Fore, Style
+
+def LOGO():
+  print(Style.BRIGHT+f"""
+
+   {Fore.CYAN}#####  ###### #####  #        ##    ####  ######          ####  #    # ###### #      #      
+   #    # #      #    # #       #  #  #    # #              {Fore.BLUE}#      #    # #      #      #      
+   #    # #####  #    # #      #    # #      #####           ####  ###### #####  #      #      
+   #####  #      #####  #      {Fore.RED}###### #      #                   # #    # #      #      #      
+   #   #  #      #      #      #    # #    # #              #    # #    # #      #      #      
+   #    # ###### #      ###### #    #  ####  {Fore.GREEN}######          ####  #    # ###### ###### ###### 
+                                                    #######                    
+    """)
+    print(Fore.BLUE+'     --{Made By: ARACADERISE}--     ')
+    print(Fore.BLUE+'     --{Version 1.0.1}--            ')
