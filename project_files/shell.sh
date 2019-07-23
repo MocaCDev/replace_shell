fa2a3c3f2149a5cc87725e186be89b98ae45e31e
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-22 22:32:58 -0500

    Update shell.sh

--- project_files/shell.sh
+++ project_files/shell.sh
@@ -112,10 +112,10 @@ ask()
     then
       pkg update && upgrade
     else
-      echo -e "$i_green \n\n==================================="
-      echo "            |    |!!!!!!!!!!!!!!!!!!!!!!!!!|   |    "
+      echo -e "$i_green \n\n ==================================="
+      echo " |    |!!!!!!!!!!!!!!!!!!!!!!!!!|   |    "
       echo -e "$i_green |   -|$i_blue INSTALLING $p_pkg|-$i_green         |"
-      echo "            |    |¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡|   |    "
+      echo " |    |¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡|   |    "
       echo -e "$i_green ===================================\n\n"
       pkg install $p_pkg
     fi
