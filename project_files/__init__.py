c4c6e199843d0ca224e3df50bdbe343e90fdfaad
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-22 21:53:31 -0500

    Create __init__.py

---
+++ project_files/__init__.py
@@ -0,0 +1,9 @@
+# Nothing special
+# Just declaring certain things
+
+AUTHOR = "ARACADERISE/GetStuffDone"
+VERSION = "1.0.1"
+STARTED = "July 10th, 2019"
+RELEASED = ""
+TYPE = "Android Platform Based, Termux"
+ANDROID_PLATS = ["Posix","Oreo","Cupcake","Pie","Kitkat"]
